97e52ae79679481aef8be510fd93310cb172b236
0CBH0 <maodatou88@163.com>
2016-04-10 03:49:07 +0800

    add new files

---
+++ cxmooc.js
@@ -0,0 +1,8 @@
+// Copyright (C) 2016  0CBH0 <maodatou88@163.com>
+// Licensed under the terms of the GNU GPL, version 3
+// http://www.gnu.org/licenses/gpl-3.0.txt
+
+var courseId = '87443251';
+var clazzid = '592538';
+var chapterId = '86886308';
+getTeacherAjax(courseId,clazzid,chapterId);
