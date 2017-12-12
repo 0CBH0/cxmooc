fc0c8ffff21bbf988ae92ccc1095316cfdc7b74e
CBH <maodatou88@163.com>
2017-12-12 23:29:24 +0800

    Update usage.js

--- usage.js
+++ usage.js
@@ -1,1 +1,2 @@
-javascript:(function(){var a=document.createElement('script');a.src='https://rawgithub.com/0CBH0/cxmooc/master/myfunc.js';document.body.appendChild(a)})();
+javascript:(function(){var a=document.createElement('script');a.src='https://rawgit.com/0CBH0/cxmooc/master/myfunc.js';document.body.appendChild(a)})();
+javascript:(function(){var a=document.createElement('script');a.src='https://rawgit.com/0CBH0/cxmooc/master/shTj.js';document.body.appendChild(a)})();
